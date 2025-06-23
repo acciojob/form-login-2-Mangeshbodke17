1ba732094ef08e52482c3ea8f4ac468d702add04
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-06-23 16:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(event){
+
+    event.preventDefault();
+
+    let inp1=document.querySelector("#First-name");
+    let inp2=document.querySelector("#Last-name");
+    let inp3=document.querySelector("#First-name");
+    let inp4=document.querySelector("#email");
+
+
+    alert(`First Name: ${inp1.value} Last Name: ${inp2.value} Phone Number: ${inp3.value} Email ID: ${inp4.value}`);
+
+
+
+})
+
